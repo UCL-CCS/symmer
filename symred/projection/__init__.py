8278b2852af1888e7564ec5442ceaa63e088ebf4
TimWeaving <weaving.timothy@gmail.com>
2022-05-10 16:16:44 +0100

    Updated CS-VQE to work with new unitary partitioning implementation

--- symred/projection/__init__.py
+++ symred/projection/__init__.py
@@ -1,6 +1,6 @@
 """init for projection."""
 from .base import S3_projection
 from .qubit_tapering import QubitTapering
-from .cs_vqe import CS_VQE, CS_VQE_LW, unitary_partitioning_rotations
+from .cs_vqe import CS_VQE, CS_VQE_LW
 from .stabilizers import HamiltonianBiasing, StabilizerIdentification
 from .build_model import build_molecule_for_projection
