Bug fixes in CS-VQE for general Hamiltonians: 1. Symmetry generator identification in CS-VQE class, no longer assumes diagonal. 2. tracking sign flips in basis reconstruction. 3. Theory of independent noncontextual basis (R is NOT independent only GuCi, this was an issue in rare cases but now fixed). 3.5 The noncontextual reconstruction over GuCi is performed separately for each clique represetative Ci to ensure no dependencies.  4. basis_reconstruction method returns the indices that were successfully reconstructed in the supplied basis. 5. The noncontextual energy is corrected for sign flips of Pauli multiplications when reconstructing terms TODO: make more efficient.

symred/symplectic/base.py
@@ -224,11 +224,18 @@ class PauliwordOp:
         the corresponding term in the operator.
         """
         dim = operator_basis.n_terms
-        basis_symp_csc = operator_basis.symp_matrix
-        basis_op_stack = np.vstack([basis_symp_csc, self.symp_matrix])
-        op_reconstruction = gf2_gaus_elim(basis_op_stack.T)[:dim,dim:].T
+        basis_symp = operator_basis.symp_matrix
+        basis_op_stack = np.vstack([basis_symp, self.symp_matrix])
+        reduced = gf2_gaus_elim(basis_op_stack.T)
 
-        return op_reconstruction
+        index_successful_reconstruction = np.where(
+            np.einsum('ij->j', reduced[dim:,dim:])==0
+        )[0]
+        #if index_unsuccessful_reconstruction:
+        #    warnings.warn(f'Terms {index_unsuccessful_reconstruction} cannot be reconstructed.')
+        op_reconstruction = reduced[:dim,dim:].T
+
+        return op_reconstruction, index_successful_reconstruction
 
     @cached_property
     def Y_count(self) -> np.array: