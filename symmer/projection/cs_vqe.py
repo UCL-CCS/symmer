174563c235b8e062632f16cd791cdefb65af72b7
TimWeaving <weaving.timothy@gmail.com>
2022-08-24 09:22:45 +0100

    Fixing outdated imports

--- symmer/projection/cs_vqe.py
+++ symmer/projection/cs_vqe.py
@@ -4,10 +4,11 @@ import warnings
 import numpy as np
 from functools import reduce
 from scipy.optimize import shgo, differential_evolution
-from symmer.symplectic.base import symplectic_to_string
-from symmer.symplectic.stabilizerop import find_symmetry_basis
 from symmer.utils import unit_n_sphere_cartesian_coords, lp_norm
-from symmer.symplectic import PauliwordOp, StabilizerOp, AntiCommutingOp
+from symmer.symplectic import (
+    PauliwordOp, StabilizerOp, AntiCommutingOp, 
+    symplectic_to_string, find_symmetry_basis
+)
 from symmer.projection import S3_projection
 
 class CS_VQE(S3_projection):
