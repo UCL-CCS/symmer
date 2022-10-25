558f1ca245240f950f8d376c3daba0403c5ddb3c
TimWeaving <weaving.timothy@gmail.com>
2022-10-25 17:03:40 +0100

    Added NoncontextualOp class, separating functionality from CS_VQE

--- symmer/symplectic/__init__.py
+++ symmer/symplectic/__init__.py
@@ -5,4 +5,5 @@ from .ansatz_op import AnsatzOp
 from .observable_op import ObservableGraph, ObservableOp
 from .stabilizer_op import StabilizerOp
 from .anticommuting_op import *
+from .noncontextual_op import *
 from .majorana_op import *
