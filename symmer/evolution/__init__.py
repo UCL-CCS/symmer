efbec424aace0a5df415a88c65578bde54bf19b4
TimWeaving <weaving.timothy@gmail.com>
2023-03-10 11:38:41 +0000

    Deleted AnsatzOp and ObservableOp - PauliwordOp_to_QuantumCircuit moved into symmer.evolution.decompositions

--- symmer/evolution/__init__.py
+++ symmer/evolution/__init__.py
@@ -1,4 +1,4 @@
 """init for evolution."""
 from .exponentiation import trotter
 from .gate_library import *
-from .decomposition import qasm_to_PauliwordOp
+from .decomposition import qasm_to_PauliwordOp, PauliwordOp_to_QuantumCircuit
--- symmer/operators/__init__.py
+++ symmer/operators/__init__.py
@@ -1,8 +1,6 @@
 """init for symplectic."""
 from .utils import *
 from .base import *
-from .ansatz_op import AnsatzOp
-from .observable_op import ObservableGraph, ObservableOp
 from .independent_op import IndependentOp
 from .anticommuting_op import AntiCommutingOp
 from .noncontextual_op import NoncontextualOp
