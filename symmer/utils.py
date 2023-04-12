c885aaa4935337ce4c7d8663f85b8b76555c266e
AlexisRalli <rallilex@hotmail.co.uk>
2023-04-12 15:18:13 +0100

    utils update and 0.0.3 version

--- symmer/utils.py
+++ symmer/utils.py
@@ -4,6 +4,9 @@ import scipy as sp
 from typing import List, Tuple, Union
 from functools import reduce
 import py3Dmol
+from scipy.sparse import csr_matrix
+from scipy.sparse import kron as sparse_kron
+import multiprocessing as mp
 
 def exact_gs_energy(
         sparse_matrix, 
@@ -193,3 +196,58 @@ def Draw_molecule(
 
     view.zoomTo()
     return view
+
+
+def get_sparse_matrix_large_pauliwordop(P_op: PauliwordOp) -> csr_matrix:
+    """
+    In order to build the sparse matrix (e.g. above 18 qubits), this function goes through each pauli term
+    divides into two equally sized tensor products finds the sparse matrix of those and then does a sparse
+    kron product to get the large matrix.
+
+    TODO:  Could also add how many junks to split problem into (e.g. three/four/... tensor products).
+
+    Args:
+        P_op (PauliwordOp): Pauli operator to convert into sparse matrix
+    Returns:
+        mat (csr_matrix): sparse matrix of P_op
+    """
+    nq = P_op.n_qubits
+    if nq<15:
+        mat = P_op.to_sparse_matrix
+    else:
+        n_cpus = mp.cpu_count()
+        P_op_chunks_inds = np.rint(np.linspace(0, P_op.n_terms, min(n_cpus, P_op.n_terms))).astype(set).astype(int)
+
+        # miss zero index out (as emtpy list)
+        P_op_chunks = [P_op[P_op_chunks_inds[ind_i]: P_op_chunks_inds[ind_i+1]] for ind_i, ind_j in enumerate(P_op_chunks_inds[1:])]
+        with mp.Pool(n_cpus) as pool:
+            tracker = pool.map(_get_sparse_matrix_large_pauliwordop, P_op_chunks)
+
+        mat = reduce(lambda x, y: x + y, tracker)
+
+    return mat
+
+
+def _get_sparse_matrix_large_pauliwordop(P_op: PauliwordOp) -> csr_matrix:
+    """
+    """
+    nq = P_op.n_qubits
+    mat = PauliwordOp.empty(nq).to_sparse_matrix
+    for op in P_op:
+        left_tensor = np.hstack((op.X_block[:, :nq // 2],
+                                 op.Z_block[:, :nq // 2]))
+        left_coeff = op.coeff_vec
+
+        right_tensor = np.hstack((op.X_block[:, nq // 2:],
+                                  op.Z_block[:, nq // 2:]))
+        right_coeff = np.array([1])
+
+        temp = sparse_kron(PauliwordOp(left_tensor, left_coeff).to_sparse_matrix,
+                           PauliwordOp(right_tensor, right_coeff).to_sparse_matrix,
+                           format='csr')  # setting format makes this faster!
+
+        mat += temp
+        del temp
+
+    return mat
+
