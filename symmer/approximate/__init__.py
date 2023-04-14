6788dcaabb669771381b641381dfb72fbebfead1
TimWeaving <weaving.timothy@gmail.com>
2023-04-14 16:39:14 +0100

    Added get_MPO function for convenience

--- symmer/approximate/__init__.py
+++ symmer/approximate/__init__.py
@@ -1,2 +1,2 @@
 """init for approximate."""
-from .tensor_network import MPOOp, find_groundstate_quimb, coefflist_to_complex
+from .tensor_network import MPOOp, get_MPO, find_groundstate_quimb, coefflist_to_complex
