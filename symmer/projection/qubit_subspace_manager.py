e64e9cc004dad97ab268969083af7787aa330c1e
TimWeaving <weaving.timothy@gmail.com>
2023-05-16 17:06:13 +0100

    Added reference state generation to QubitSubspaceManager

--- symmer/projection/qubit_subspace_manager.py
+++ symmer/projection/qubit_subspace_manager.py
@@ -1,5 +1,7 @@
 from symmer.operators import PauliwordOp, QuantumState
 from symmer.projection import QubitTapering, ContextualSubspace
+from symmer.utils import exact_gs_energy
+from symmer.approximate import get_MPO, find_groundstate_quimb
 from typing import Union, List
 import numpy as np
 import warnings
@@ -10,25 +12,47 @@ class QubitSubspaceManager:
 
     def __init__(self,
             hamiltonian: PauliwordOp,
-            ref_state: Union[np.array, List[int], QuantumState] = None,
+            ref_state: Union[np.ndarray, List[int], QuantumState] = None,
             run_qubit_tapering: bool = True,
             run_contextual_subspace: bool = True
         ) -> None:
         """
         """
         self.hamiltonian = hamiltonian
-        self.ref_state = ref_state
+        self.ref_state = self.prepare_ref_state(ref_state)
         self.run_qubit_tapering = run_qubit_tapering
         self.run_contextual_subspace = run_contextual_subspace
         self.build_subspace_objects()
         
+    def prepare_ref_state(self, ref_state=None):
+        """
+        """
+        if ref_state is not None:
+            if isinstance(ref_state, list):
+                ref_state = np.array(ref_state).reshape(-1)
+            if isinstance(ref_state, np.ndarray):
+                ref_state = QuantumState(ref_state, [1])
+        else:
+            warnings.warn('No reference state supplied - trying to identify one via alternative means.')
+            if self.hamiltonian.n_qubits <= 12:
+                _, ref_state = exact_gs_energy(self.hamiltonian.to_sparse_matrix)
+            else:
+                warnings.warn(
+                    'Results are currently unstable for reference state '+ 
+                    'generation via tensor network techniques'
+                )
+                mpo = get_MPO(self.hamiltonian, max_bond_dimension=10)
+                ref_state = find_groundstate_quimb(mpo)
+
+        return ref_state.cleanup(zero_threshold=1e-4).normalize
+
     def build_subspace_objects(self):
         """
         """
         if self.run_qubit_tapering:
             self.QT           = QubitTapering(operator=self.hamiltonian)
             self._hamiltonian = self.QT.taper_it(ref_state=self.ref_state)
-            self._ref_state   = self.QT.tapered_ref_state
+            self._ref_state   = self.QT.tapered_ref_state.normalize
         else:
             self._hamiltonian = self.hamiltonian.copy()
             self._ref_state   = self.ref_state.copy()
