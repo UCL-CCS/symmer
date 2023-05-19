8ecb4a1a854c49103209361986a19e0112154709
TimWeaving <weaving.timothy@gmail.com>
2023-05-19 11:25:01 +0100

    Testing the QubitSubspaceManager

--- symmer/projection/qubit_subspace_manager.py
+++ symmer/projection/qubit_subspace_manager.py
@@ -26,7 +26,7 @@ class QubitSubspaceManager:
     Hamiltonian is sufficiently small, or using a DMRG calculation.
     """
 
-    CS_ready = False
+    _projection_ready = False
 
     def __init__(self,
             hamiltonian: PauliwordOp,
@@ -73,12 +73,14 @@ class QubitSubspaceManager:
         """ Initialize the relevant qubit subspace classes.
         """
         if self.run_qubit_tapering:
-            self.QT           = QubitTapering(operator=self.hamiltonian)
-            self._hamiltonian = self.QT.taper_it(ref_state=self.ref_state)
-            self._ref_state   = self.QT.tapered_ref_state.normalize
+            self.QT             = QubitTapering(operator=self.hamiltonian)
+            self._hamiltonian   = self.QT.taper_it(ref_state=self.ref_state)
+            self._ref_state     = self.QT.tapered_ref_state.normalize
+            self._Z2_symmetries = self.QT.symmetry_generators.copy()
         else:
-            self._hamiltonian = self.hamiltonian.copy()
-            self._ref_state   = self.ref_state.copy()
+            self._hamiltonian   = self.hamiltonian.copy()
+            self._ref_state     = self.ref_state.copy()
+            self._Z2_symmetries = None
 
         if self.run_contextual_subspace:
             self.CS = ContextualSubspace(
@@ -94,46 +96,75 @@ class QubitSubspaceManager:
         """ Project the Hamiltonian in line with the desired qubit subspace techqniques
         and, in the case of ContextualSubspace, the desired number of qubits.
         """
+        self._projection_ready = True
+        self._n_qubits = n_qubits
         if aux_operator is None:
             aux_operator = self._aux_operator
 
-        if self.run_qubit_tapering:
-            if not self.run_contextual_subspace and n_qubits is not None:
-                warnings.warn('The n_qubits parameter is redundant when contextual subspace is not run.')
-            operator_out = self._hamiltonian
-            aux_operator = self.QT.taper_it(aux_operator=aux_operator)
+        if n_qubits >= self.hamiltonian.n_qubits:
+            warnings.warn(
+                'Specified at least as many qubits as are present in the Hamiltonian - '+
+                f'returning the full {self.hamiltonian.n_qubits} operator.')
+            operator_out = self.hamiltonian
 
-        if self.run_contextual_subspace:
-            assert n_qubits is not None, 'Must supply the desired number of qubits for the contextual subspace.'
-            self.CS.update_stabilizers(
-                n_qubits=n_qubits, aux_operator=aux_operator, strategy='aux_preserving'
-            )
-            self.CS_ready = True
-            operator_out = self.CS.project_onto_subspace()
+        elif n_qubits > self._hamiltonian.n_qubits:
+            # if one wishes not to taper all the available Z2 symmetries
+            assert self.run_qubit_tapering, ''
+            self.QT.symmetry_generators = self._Z2_symmetries[:self.hamiltonian.n_qubits-n_qubits]
+            operator_out = self.QT.taper_it(ref_state=self.ref_state)
+
+        else:
+            if self.run_qubit_tapering:
+                if not self.run_contextual_subspace and n_qubits < self._hamiltonian.n_qubits:
+                    warnings.warn(
+                        'When contextual subspace is not run we may only reduce '+ 
+                        'the Hamiltonian by the number of Z2 symmetries present. '+
+                        f'The reduced Hamiltonian will contain {self._hamiltonian.n_qubits} qubits.'
+                    )
+                self.QT.symmetry_generators = self._Z2_symmetries
+                aux_operator = self.QT.taper_it(aux_operator=aux_operator)
+                operator_out = self._hamiltonian
+
+            if self.run_contextual_subspace:
+                assert n_qubits is not None, 'Must supply the desired number of qubits for the contextual subspace.'
+                self.CS.update_stabilizers(
+                    n_qubits=n_qubits, aux_operator=aux_operator, strategy='aux_preserving'
+                )
+                operator_out = self.CS.project_onto_subspace()
+
+            if not self.run_qubit_tapering and not self.run_contextual_subspace:
+                warnings.warn('Not running any subspace methods - returning the original Hamiltonian')
+                operator_out = self.hamiltonian
 
         return operator_out
         
     def project_auxiliary_operator(self, operator: PauliwordOp) -> PauliwordOp:
         """ Project additional operators consistently with respect to the Hamiltonian.
         """
-        if self.run_qubit_tapering:
-            operator = self.QT.taper_it(aux_operator=operator)
-        
-        if self.run_contextual_subspace:
-            assert self.CS_ready, 'Have not yet projected the Hamiltonian into the contextual subspace'
-            operator = self.CS.project_onto_subspace(operator_to_project=operator)
+        assert self._projection_ready, 'Have not yet projected the Hamiltonian into the contextual subspace'
+
+        if self._n_qubits < self.hamiltonian.n_qubits:
+
+            if self.run_qubit_tapering:
+                operator = self.QT.taper_it(aux_operator=operator)
+            
+            if self.run_contextual_subspace:
+                operator = self.CS.project_onto_subspace(operator_to_project=operator)
 
         return operator
     
     def project_auxiliary_state(self, state: QuantumState) -> QuantumState:
         """ Project additional operators consistently with respect to the Hamiltonian.
         """
-        if self.run_qubit_tapering:
-            state = self.QT.project_state(state)
-        
-        if self.run_contextual_subspace:
-            assert self.CS_ready, 'Have not yet projected the Hamiltonian into the contextual subspace'
-            state = self.CS.project_state_onto_subspace(state_to_project=state)
+        assert self._projection_ready, 'Have not yet projected the Hamiltonian into the contextual subspace'
+
+        if self._n_qubits < self.hamiltonian.n_qubits:
+
+            if self.run_qubit_tapering:
+                state = self.QT.project_state(state)
+            
+            if self.run_contextual_subspace:
+                state = self.CS.project_state_onto_subspace(state_to_project=state)
 
         return state
 
