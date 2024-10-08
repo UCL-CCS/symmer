9e78d578a59c49ac557d5be052f098e35f1f402f
AlexisRalli <rallilex@hotmail.co.uk>
2024-10-08 13:22:43 -0400

    removed cotengra UserWarning at import

--- symmer/__init__.py
+++ symmer/__init__.py
@@ -1,4 +1,6 @@
 """Main init for package."""
+import warnings
+warnings.filterwarnings('ignore', module='cotengra')
 from symmer.process_handler import process
 from symmer.operators  import PauliwordOp, QuantumState
 from symmer.projection import QubitTapering, ContextualSubspace, QubitSubspaceManager
